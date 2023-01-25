78bc22586a94643b3deba385d6d148f62d1ef67f
Josh Blad <jpb82@byu.edu>
2023-01-25 14:14:50 -0700

    Made MVC model for Assignment 4 work

---
+++ Mission4Assignment/Models/GradeCalculatorModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4Assignment.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        public int Assignments { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        public int Group { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        public int Quizzes { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        public int Midterm { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        public int Final { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        public int Intex { get; set; }
+
+    }
+}
