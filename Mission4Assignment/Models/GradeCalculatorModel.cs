374e8f7101d687b9eae028e36c1d6bc98b909609
Josh Blad <jpb82@byu.edu>
2023-01-26 07:24:41 -0700

    Trying to get js to work

--- Mission4Assignment/Models/GradeCalculatorModel.cs
+++ Mission4Assignment/Models/GradeCalculatorModel.cs
@@ -9,27 +9,27 @@ namespace Mission4Assignment.Models
     public class GradeCalculatorModel
     {
         [Required]
-        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        // [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
         public int Assignments { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        // [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
         public int Group { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        // [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
         public int Quizzes { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        // [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
         public int Midterm { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        // [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
         public int Final { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
+        // [Range(0, 100, ErrorMessage = "Grade must be between 0% and 100%")]
         public int Intex { get; set; }
 
     }
